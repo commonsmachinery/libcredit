465eb90cd8f19b45300c3d6e968d04c8aec6626f
Peter Liljenberg <peter.liljenberg@gmail.com>
2013-11-05 17:37:33 +0100

    Rename python package to libcredit.py to separate it from the JS one

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 from DistUtilsExtra.command import (build_extra, build_i18n)
 
 setup(
-    name='libcredit',
-    version='0.1',
+    name='libcredit.py',
+    version='0.1.0',
     packages=['libcredit'],
     package_dir = { '': 'python' },
     cmdclass={
