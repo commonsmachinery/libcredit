41718c3200f1e0a5e3383dbce2940ff6150e8959
Peter Liljenberg <peter.liljenberg@gmail.com>
2013-11-05 18:26:02 +0100

    Change python impl from package to module (and fix the gettext domain)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from DistUtilsExtra.command import (build_extra, build_i18n)
 setup(
     name='libcredit.py',
     version='0.1.0',
-    packages=['libcredit'],
+    py_modules=['libcredit'],
     package_dir = { '': 'python' },
     cmdclass={
         "build": build_extra.build_extra,
