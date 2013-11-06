ca40f0739e2cdf1ce31fc4de413769f69e46e65e
Peter Liljenberg <peter.liljenberg@gmail.com>
2013-11-06 14:57:12 +0100

    API restructure, and first RDF parsing

--- javascript/libcredit.js
+++ javascript/libcredit.js
@@ -12,15 +12,51 @@
 
     var libcredit = {};
 
+    // Namespace used in the code
+    var DC = $rdf.Namespace('http://purl.org/dc/elements/1.1/');
+    const DCTERMS = $rdf.Namespace('http://purl.org/dc/terms/');
+    const CC = $rdf.Namespace('http://creativecommons.org/ns#');
+    const XHTML = $rdf.Namespace('http://www.w3.org/1999/xhtml/vocab#');
+    const OG = $rdf.Namespace('http://ogp.me/ns#');
+
+    var getCreditLine = function(hasTitle, hasAttrib, hasLicense)
+    {
+	if (hasTitle) {
+	    if (hasAttrib) {
+		return (hasLicense ?
+			'<title> by <attrib> (<license>)' : 
+			'<title> by <attrib>');
+	    }
+	    else {
+		return (hasLicense ?
+			'<title> (<license>)' :
+			'<title>');
+	    }
+	}
+	else {
+	    if (hasAttrib) {
+		return (hasLicense ?
+			'Credit: <attrib> (<license>)' :
+			'Credit: <attrib>');
+	    }
+	    else {
+		return (hasLicense ?
+			'License: <license>' :
+			null);
+	    }
+	}
+    };
+
+
     //
     // Public API
     //
 
     /* credit(rdf, [subjectURI])
      *
-     * Return a new object that can format
-     * credit strings using RDF metadata.  The RDF graph is parsed at
-     * construction time.
+     * Return a new object that contain the credit information
+     * extracted from the RDF metadata, or null if there are no 
+     * information.
      *
      * Parameters:
      *
@@ -33,7 +69,95 @@
      */
 
     var credit = function(rdf, subjectURI) {
+	var kb;
+
+	var titleText = null;
+	var titleURL = null;
+	var attribText = null;
+	var attribURL = null;
+	var licenseText = null;
+	var licenseURL = null;
+	var sources = [];
+
+	var getTextProperty = function(subject, predicate) {
+	    var objs, i;
+
+	    objs = kb.each(subject, predicate, null);
+	    
+	    for (i = 0; i < objs.length; i++) {
+		if (objs[i].termType === 'literal') {
+		    // TODO: check xml:lang
+		    return objs[i].value;
+		}
+		// TODO: parse rdf:Alt 
+	    }
+
+	    return null;
+	};
+
+	// Make scope a little cleaner by putting the parsing into
+	// it's own function
+
+	var parse = function() {
+	    var mainSource, subject, v;
+
+	    if (subjectURI === null || subjectURI === undefined) {
+		// Locate using <> <dc:source> ?, which is how the
+		// Copy RDFa firefox addon indicates the original of
+		// the copied object
+
+		mainSource = kb.any(kb.sym(''), DC('source'));
+
+		if (mainSource && mainSource.uri) {
+		    subject = mainSource;
+		}
+		else {
+		    // No clue what the source might be, so give up
+		    return false;
+		}
+	    }
+	    else {
+		subject = kb.sym(subjectURI);
+	    }
+
+	    titleText = getTextProperty(subject, DC('title'));
+
+	    // Did we manage to get anything that can make it into a credit?
+	    return (titleText || attribText || licenseText || sources.length > 0);
+	};
+
+
+	// TODO: parse RDF
+	kb = rdf;
+
+	if (!parse()) {
+	    return null;
+	}
+
 	var that = {};
+
+	/* credit.getTitleText():
+	 * credit.getTitleURL():
+	 * credit.getAttribText():
+	 * credit.getAttribURL():
+	 * credit.getLicenseText():
+	 * credit.getLicenseURL():
+	 *
+	 * Property getters returning a string or null.
+	 */
+	
+	/* credit.getSources():
+	 *
+	 * Property getter returning an array of credit objects:
+	 */
+
+	that.getTitleText = function() { return titleText; };
+	that.getTitleURL = function() { return titleURL; };
+	that.getAttribText = function() { return attribText; };
+	that.getAttribURL = function() { return attribURL; };
+	that.getLicenseText = function() { return licenseText; };
+	that.getLicenseURL = function() { return licenseURL; };
+	that.getSources = function() { return sources.slice(0); };
 
     /* credit.format(formatter, [i18n])
      *
@@ -51,7 +175,8 @@
      *   language into it.
      */
 
-	that.format = function(formatter) {
+	that.format = function(formatter, i18n) {
+	    var creditLine = getCreditLine(!!titleText, !!attribText, !!licenseText);
 	    
 	};
 
@@ -67,13 +192,11 @@
      */
     var creditFormatter = function() {
 	var that = {};
-
-	that.begin = function() {};
-	that.end = function() {};
+	
+	that.begin_credit = function() {};
+	that.end_credit = function() {};
 	that.begin_sources = function(label) {};
 	that.end_sources = function() {};
-	that.begin_source = function() {};
-	that.end_source = function() {};
 	that.add_title = function(text, url) {};
 	that.add_attrib = function(text, url) {};
 	that.add_license = function(text, url) {};
@@ -83,68 +206,6 @@
     };
     libcredit.creditFormatter = creditFormatter;
 
-
-    /* objectCreditFormatter()
-     *
-     * Collect the credit information into a object, rather than
-     * generating a human-consumable credit message.
-     */  
-    var objectCreditFormatter = function() {
-	var that = creditFormatter();
-
-	var current = null;
-	var sourceStack = null;
-
-	var emptyCredit = function() {
-	    return {
-		titleText: null,
-		titleURL: null,
-		attribText: null,
-		attribURL: null,
-		licenseText: null,
-		licenseURI: null,
-		sources: []
-	    };
-	};
-		
-	that.getCredit = function() {
-	    return current;
-	};
-
-	that.begin = function() {
-	    current = emptyCredit();
-	    sourceStack = [];
-	};
-
-	that.begin_source = function() {
-	    var source = emptyCredit();
-	    current.sources.push(source);
-	    sourceStack.push(current);
-	    current = source;
-	};
-
-	that.end_source = function() {
-	    current = sourceStack.pop();
-	};
-
-	that.add_title = function(text, url) {
-	    current.titleText = text;
-	    current.titleURL = url;
-	};
-
-	that.add_attrib = function(text, url) {
-	    current.attribText = text;
-	    current.attribURL = url;
-	};
-	    
-	that.add_license = function(text, url) {
-	    current.licenseText = text;
-	    current.licenseURL = url;
-	};
-
-	return that;
-    };
-    libcredit.objectCreditFormatter = objectCreditFormatter;
 
     var textCreditFormatter = function() {
 	var that = creditFormatter();
--- javascript/test/tests.js
+++ javascript/test/tests.js
@@ -1,19 +1,20 @@
 (function (libcredit) {
 
     var testCredit = function(filename, func) {
-	it('credits ' + filename, function() {
+	it('credit for ' + filename, function() {
 	    var str, kb, credit, formatter;
 
 	    str = fs.readFileSync('../test_input/' + filename + '.ttl', 'utf-8');
 	    
 	    kb = new $rdf.IndexedFormula();
-	    $rdf.parse(str, kb, 'urn:test', 'text/turtle');
 
-	    credit = libcredit.credit(kb);
-	    formatter = libcredit.objectCreditFormatter();
-	    credit.format(formatter);
-	    
-	    func(formatter.getCredit());
+	    // A bug in rdflib means that we must pass in a base URI,
+	    // '' triggers an error
+	    $rdf.parse(str, kb, 'urn:base', 'text/turtle');
+
+	    // So because of that bug, we have to specify the source URI
+	    credit = libcredit.credit(kb, 'urn:src');
+	    func(credit);
 	});
     };
 
@@ -25,7 +26,6 @@
 	it("should have public functions", function () {
 	    expect( libcredit.credit ).to.be.a('function');
 	    expect( libcredit.creditFormatter ).to.be.a('function');
-	    expect( libcredit.objectCreditFormatter ).to.be.a('function');
 	    expect( libcredit.textCreditFormatter ).to.be.a('function');
 	    expect( libcredit.htmlCreditFormatter ).to.be.a('function');
 	});
@@ -37,6 +37,16 @@
 	    expect( credit ).to.be( null );
 	});
 
+	testCredit('dc-title-text', function (credit) {
+	    expect( credit ).to.not.be( null );
+	    expect( credit.getTitleText()	).to.be( 'a title' );
+	    expect( credit.getTitleURL()	).to.be( null );
+	    expect( credit.getAttribText()	).to.be( null );
+	    expect( credit.getAttribURL()	).to.be( null );
+	    expect( credit.getLicenseText()	).to.be( null );
+	    expect( credit.getLicenseURL()	).to.be( null );
+	    expect( credit.getSources().length	).to.be( 0 );
+	});
     });
 
 })(libcredit);
