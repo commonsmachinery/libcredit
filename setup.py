6a27fd106ef7b4580ab75495b8ee4f5571e1beac
Peter Liljenberg <peter.liljenberg@gmail.com>
2013-11-15 17:06:35 +0100

    [python] Provide more package information

--- setup.py
+++ setup.py
@@ -6,10 +6,24 @@ from DistUtilsExtra.command import (build_extra, build_i18n)
 setup(
     name='libcredit.py',
     version='0.1.0',
+
+    url = 'https://github.com/commonsmachinery/libcredit',
+    author = 'Commons Machinery http://commonsmachinery.se/',
+    author_email = '<dev@commonsmachinery.se>',
+    description = 'Generate attribution and license messages from RDF metadata',
+    license = 'GPLv2',
+
     py_modules=['libcredit'],
     package_dir = { '': 'python' },
     cmdclass={
         "build": build_extra.build_extra,
         "build_i18n": build_i18n.build_i18n,
     },
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        ]
 )
